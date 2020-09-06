4c161a95a7d348323a0e098fe614aebe952cd831
seiyoungahn <seddong@students.cs.ubc.ca>
2020-09-06 16:58:21 -0700

    Update main.js to include authentication

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const extractAuthCode = require('./extractAuthCode');
 
-let win;
+let mainWindow, authWindow;
+
+app.disableHardwareAcceleration()
+
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({
-    width: 600, 
-    height: 600,
-    backgroundColor: '#ffffff',
+  mainWindow = new BrowserWindow({
+    width: 500, 
+    height: 500,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    frame: false,
+    transparent: true
     // icon: `file://${__dirname}/dist/assets/logo.png`
   })
-
-
-  win.loadURL(`file://${__dirname}/dist/spotify-floats/index.html`)
+  mainWindow.setAlwaysOnTop(true, 'floating', 1);
+  mainWindow.setVisibleOnAllWorkspaces(true);
+  mainWindow.setFullScreenable(false);
+  // mainWindow.webContents.openDevTools()
+  
+  mainWindow.loadURL(`file://${__dirname}/dist/spotify-floats/index.html`)
 
   //// uncomment below to open the DevTools.
   // win.webContents.openDevTools()
 
   // Event when the window is closed.
-  win.on('closed', function () {
-    win = null
+  mainWindow.on('closed', _ => {
+    mainWindow = null
   })
 }
 
+function onReady() {
+  setTimeout(() => createWindow(), 1000);
+}
+
 // Create window on electron intialization
-app.on('ready', createWindow)
+app.on('ready', onReady);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', _ => {
 
   // On macOS specific close process
   if (process.platform !== 'darwin') {
@@ -35,9 +50,33 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', _ => {
   // macOS specific close process
-  if (win === null) {
+  if (mainWindow === null) {
     createWindow()
   }
 })
+
+ipcMain.on('request-auth', (event, authURL) => {
+  mainWindow.webContents.send('pong', 'pong');
+  authWindow = new BrowserWindow();
+  authWindow.removeMenu();
+  authWindow.loadURL(authURL);
+  authWindow.on('closed', _ => {
+    authWindow = null;
+  });
+  authWindow.webContents.on('will-redirect', function (event, URL) {
+    const redirectURL = 'https://www.google.com/';
+    if (URL.includes(redirectURL)) {
+      console.log('redirecting');
+      console.log(URL);
+      let authCode = extractAuthCode(URL);
+      console.log(authCode);
+      mainWindow.webContents.send('auth-return', authCode);
+      authWindow.destroy();
+      authWindow = null;
+    } else {
+      console.log('still in spotify');
+    }
+  });
+});
