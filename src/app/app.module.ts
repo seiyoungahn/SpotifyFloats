37769344101671c066c60ff6913c56bd8d9416a1
seiyoungahn <seddong@students.cs.ubc.ca>
2020-09-06 16:39:11 -0700

    Add IPCService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TokenService } from './services/token.service';
+import { PersistenceService } from './services/persistence.service';
+import { IpcService } from './services/ipc.service';
+import { APIService } from './services/api.service';
+import { PlayerService } from './services/player.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    SettingsComponent,
+    SliderComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    PersistenceService,
+    TokenService,
+    AuthenticationService,
+    IpcService,
+    APIService,
+    PlayerService,
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
