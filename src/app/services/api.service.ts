e2a7eb2e0e2033e9abbf4cdea698b40306b7f656
seiyoungahn <seddong@students.cs.ubc.ca>
2020-09-06 16:13:08 -0700

    Add requestTokens method for APIService

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+import { CLIENT_ID, CLIENT_SECRET } from '../app.config';
 import { IpcService } from './ipc.service';
 import { TokenService } from './token.service';
 
@@ -11,5 +13,26 @@ export class APIService {
   constructor(private http: HttpClient,
               private ipcService: IpcService,
               private tokenService: TokenService) {
-              }
+  }
+
+  private requestTokens(code: string): Promise<ITokenResponse> {
+    // console.log(code);
+    const URL = 'https://accounts.spotify.com/api/token';
+    const redirectURL = 'https://www.google.com/';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }),
+      params: new HttpParams()
+        .set("grant_type", 'authorization_code')
+        .set("code", code)
+        .set("redirect_uri", redirectURL)
+    }
+    return this.http.post<ITokenResponse>(
+      URL,
+      null,
+      httpOptions
+    ).toPromise();
+  }      
 }
