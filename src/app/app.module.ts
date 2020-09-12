5281f444c8c470623079931c3aa5a2d11c369b89
seiyoungahn <seddong@students.cs.ubc.ca>
2020-09-11 18:22:39 -0700

    Use TokenInterceptor in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
@@ -11,6 +11,7 @@ import { PersistenceService } from './services/persistence.service';
 import { AuthenticationService } from './services/authentication.service';
 import { IpcService } from './services/ipc.service';
 import { APIService } from './services/api.service';
+import { TokenInterceptor } from './services/token-interceptor.service';
 import { PlayerService } from './services/player.service';
 import { SettingsComponent } from './components/settings.component';
 import { SettingsService } from './services/settings.service';
@@ -34,6 +35,11 @@ const appRoutes: Routes = [
     FontAwesomeModule
   ],
   providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    },
     PersistenceService,
     TokenService,
     AuthenticationService,
