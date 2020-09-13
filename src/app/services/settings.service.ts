67cbee940022fdaab932afb61405ebe0498af293
seiyoungahn <seddong@students.cs.ubc.ca>
2020-09-12 21:04:38 -0700

    Implement setVolume method to SettingsService

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+import { APIService } from './api.service'
+
 /**
  * Keeps track of settings values
  */
@@ -8,8 +10,9 @@ import { BehaviorSubject } from 'rxjs';
 export class SettingsService {
   public size: BehaviorSubject<number> = new BehaviorSubject(15); // font size in px
   public opacity: BehaviorSubject<number> = new BehaviorSubject(100); // range from 0-100
+  public volume: BehaviorSubject<number> = new BehaviorSubject(100);
 
-  constructor() {}
+  constructor(private apiService: APIService) {}
 
   public setSize(size: number) {
     this.size.next(size);
@@ -17,5 +20,14 @@ export class SettingsService {
 
   public setOpacity(opacity: number) {
     this.opacity.next(opacity);
+  }
+
+  public setVolume(value: number): Promise<void> {
+    const currentVolume = this.volume.value;
+    this.volume.next(value);
+    return this.apiService.setVolume(value).catch(
+        // if it catches error, revert to original volume
+        () => this.volume.next(currentVolume)
+      );
   }
 }
